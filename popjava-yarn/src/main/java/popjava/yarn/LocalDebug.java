f94d0c931e3f419bddd2cc9a5f81421405dfc39e
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-09 13:25:48 -0700

    Alternative to locally run AppMaster

---
+++ popjava-yarn/src/main/java/popjava/yarn/LocalDebug.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package popjava.yarn;
+
+import popjava.PopJava;
+import popjava.jobmanager.POPJavaJobManager;
+import popjava.system.POPSystem;
+import popjava.yarn.command.POPAppStatus;
+import popjava.yarn.command.TaskServer;
+
+/**
+ *
+ * @author Dosky
+ */
+public class LocalDebug {
+    public static void main(String[] args) throws InterruptedException {
+        TaskServer taskServer;
+        POPJavaJobManager jobManager;
+        
+        POPSystem.initialize();
+        taskServer = PopJava.newActive(TaskServer.class);
+        jobManager = PopJava.newActive(POPJavaJobManager.class);
+        
+        taskServer.setJobManager(jobManager.getAccessPoint());
+        // server status, waiting
+        taskServer.setStatus(POPAppStatus.WAITING);
+        
+        System.out.println(taskServer.getAccessPoint());
+        
+        Thread.sleep(30000);
+        
+        taskServer.setStatus(POPAppStatus.FINISHED);
+        
+        Thread.sleep(10000);
+        
+        POPSystem.end();
+    }
+}
