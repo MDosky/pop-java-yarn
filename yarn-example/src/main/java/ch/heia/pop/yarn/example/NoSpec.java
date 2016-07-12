978e77909cbb51da44929b41b58213d5ed810543
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-11 22:44:19 -0700

    remove throw in main

--- yarn-example/src/main/java/ch/heia/pop/yarn/example/NoSpec.java
+++ yarn-example/src/main/java/ch/heia/pop/yarn/example/NoSpec.java
@@ -1,5 +1,7 @@
 package ch.heia.pop.yarn.example;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import popjava.PopJava;
 import popjava.annotation.POPClass;
 import popjava.annotation.POPSyncSeq;
@@ -12,40 +14,44 @@ import popjava.system.POPSystem;
  */
 public class NoSpec {
    
-    public static void main(String[] args) throws InterruptedException {
-        POPSystem.initialize(args);
-        
-        System.out.println("Starting pop java app");
-        AAA aaa;
-        
-        System.out.println("first");
-        aaa = PopJava.newActive(AAA.class);
-        System.out.println(aaa.aaa());
-        aaa.exit();
-        Thread.sleep(5000);
-        System.out.println("second");
-        aaa = PopJava.newActive(AAA.class);
-        System.out.println(aaa.aaa());
-        aaa.exit();
-        Thread.sleep(5000);
-        System.out.println("third");
-        aaa = PopJava.newActive(AAA.class);
-        System.out.println(aaa.aaa());
-        aaa.exit();
-        Thread.sleep(5000);
-        System.out.println("forth");
-        aaa = PopJava.newActive(AAA.class);
-        System.out.println(aaa.aaa());
-        aaa.exit();
-        Thread.sleep(5000);
-        System.out.println("fifth");
-        aaa = PopJava.newActive(AAA.class);
-        System.out.println(aaa.aaa());
-        aaa.exit();
-        
-        System.out.println("end app");
-        POPSystem.end();
-        System.exit(0);
+    public static void main(String[] args) {
+        try {
+            POPSystem.initialize(args);
+            
+            System.out.println("Starting pop java app");
+            AAA aaa;
+            
+            System.out.println("first");
+            aaa = PopJava.newActive(AAA.class);
+            System.out.println(aaa.aaa());
+            aaa.exit();
+            Thread.sleep(5000);
+            System.out.println("second");
+            aaa = PopJava.newActive(AAA.class);
+            System.out.println(aaa.aaa());
+            aaa.exit();
+            Thread.sleep(5000);
+            System.out.println("third");
+            aaa = PopJava.newActive(AAA.class);
+            System.out.println(aaa.aaa());
+            aaa.exit();
+            Thread.sleep(5000);
+            System.out.println("forth");
+            aaa = PopJava.newActive(AAA.class);
+            System.out.println(aaa.aaa());
+            aaa.exit();
+            Thread.sleep(5000);
+            System.out.println("fifth");
+            aaa = PopJava.newActive(AAA.class);
+            System.out.println(aaa.aaa());
+            aaa.exit();
+            
+            System.out.println("end app");
+            POPSystem.end();
+            System.exit(0);
+        } catch (InterruptedException ex) {
+            Logger.getLogger(NoSpec.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
 
     
