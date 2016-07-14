b8573d930096f6264835e142922700404a4ded6d
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-13 18:45:06 -0700

    Making the AM a pop object we can call anytime to allocate a container

---
+++ popjava-yarn/src/main/java/popjava/yarn/ApplicationMasterPOP.java
@@ -0,0 +1,160 @@
+package popjava.yarn;
+
+import com.beust.jcommander.Parameter;
+import com.google.common.collect.Lists;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.yarn.api.records.FinalApplicationStatus;
+import org.apache.hadoop.yarn.api.records.Priority;
+import org.apache.hadoop.yarn.api.records.Resource;
+import org.apache.hadoop.yarn.client.api.AMRMClient;
+import org.apache.hadoop.yarn.client.api.NMClient;
+import org.apache.hadoop.yarn.client.api.async.AMRMClientAsync;
+import org.apache.hadoop.yarn.conf.YarnConfiguration;
+import org.apache.hadoop.yarn.util.Records;
+import popjava.PopJava;
+import popjava.annotation.POPAsyncConc;
+import popjava.annotation.POPClass;
+import popjava.annotation.POPSyncConc;
+import popjava.annotation.POPSyncSeq;
+
+/**
+ *
+ * @author Dosky
+ */
+@POPClass
+public class ApplicationMasterPOP {
+
+    private Configuration configuration;
+    private NMClient nmClient;
+    
+    private ApplicationMasterRMCallback rmCallback;
+    private AMRMClientAsync<AMRMClient.ContainerRequest> rmClient;
+
+    private Process popProcess;
+    private String taskServer;
+    private String jobManager;
+    
+    private int requestedContainers;
+
+    @Parameter(names = "--dir", required = true)
+    private String hdfs_dir;
+    @Parameter(names = "--vcores", required = true)
+    private int vcores;
+    @Parameter(names = "--memory", required = true)
+    private int memory;
+    @Parameter(names = "--containers", required = true)
+    private int askedContainers;
+    @Parameter(names = "--main", required = true)
+    private String main;
+    @Parameter
+    private final List<String> args = new ArrayList<>();
+    
+    
+    public ApplicationMasterPOP() {
+    }
+
+    @POPSyncSeq
+    public void setup() {
+        configuration = new YarnConfiguration();
+        
+        rmCallback = new ApplicationMasterRMCallback(hdfs_dir, askedContainers, main, args);
+        
+        nmClient = NMClient.createNMClient();
+        nmClient.init(configuration);
+        nmClient.start();
+        
+        rmClient = AMRMClientAsync.createAMRMClientAsync(100, rmCallback);
+        rmClient.init(configuration);
+        rmClient.start();
+        
+        rmCallback.setup(nmClient);
+        
+        // start as thread
+        PopJava.getThis(this).startCentralServers();
+    }
+
+    @POPSyncConc
+    public void runMainLoop() throws Exception {
+        // wait for Central Servers to be up
+        Thread.sleep(5000);
+
+        // Register with ResourceManager
+        System.out.println("[AM] registerApplicationMaster 0");
+        rmClient.registerApplicationMaster("", 0, "");
+        System.out.println("[AM] registerApplicationMaster 1");
+
+        for (int i = 0; i < askedContainers; i++) {
+            PopJava.getThis(this).requestContainer(memory, vcores);
+        }
+
+        System.out.println("[AM] waiting for containers to finish");
+        while (!rmCallback.doneWithContainers()) {
+            Thread.sleep(100);
+        }
+
+        System.out.println("[AM] unregisterApplicationMaster 0");
+        // Un-register with ResourceManager
+        rmClient.unregisterApplicationMaster(FinalApplicationStatus.SUCCEEDED, "", "");
+        System.out.println("[AM] unregisterApplicationMaster 1");
+    }
+    
+    /**
+     * Request a new container
+     * @param memory
+     * @param vcores 
+     */
+    @POPAsyncConc
+    public void requestContainer(int memory, int vcores) {
+        // Priority for worker containers - priorities are intra-application
+        Priority priority = Records.newRecord(Priority.class);
+        priority.setPriority(0);
+
+        // Resource requirements for worker containers
+        Resource capability = Records.newRecord(Resource.class);
+        capability.setMemory(memory);
+        capability.setVirtualCores(vcores);
+
+        // Make container requests to ResourceManager
+        AMRMClient.ContainerRequest containerAsk = new AMRMClient.ContainerRequest(capability, null, null, priority);
+        System.out.println("[AM] Making reservation request " + requestedContainers++);
+        rmClient.addContainerRequest(containerAsk);
+    }
+
+    /**
+     * Start a clean version of POP-Java which will be used to create processes.
+     * Using the same as the one the AM run on pollute the classpath.
+     */
+    @POPAsyncConc
+    private void startCentralServers() {
+        List<String> popServer = Lists.newArrayList(
+                System.getProperty("java.home") + "/bin/java",
+                "-javaagent:popjava.jar",
+                "-cp", "popjava.jar:pop-app.jar",
+                "popjava.yarn.ApplicationMasterPOPServer"
+        );
+
+        ProcessBuilder pb = new ProcessBuilder(popServer);
+
+        try {
+            popProcess = pb.start();
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(popProcess.getInputStream()))) {
+                String out;
+                while (!(taskServer = reader.readLine()).startsWith(ApplicationMasterPOPServer.TASK));
+                taskServer = taskServer.substring(ApplicationMasterPOPServer.TASK.length());
+                while (!(jobManager = reader.readLine()).startsWith(ApplicationMasterPOPServer.JOBM));
+                jobManager = jobManager.substring(ApplicationMasterPOPServer.JOBM.length());
+
+                while ((out = reader.readLine()) != null) {
+                    System.err.println(out);
+                }
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+}
---
+++ popjava-yarn/src/main/java/popjava/yarn/ApplicationMasterRMCallback.java
@@ -0,0 +1,146 @@
+package popjava.yarn;
+
+import com.google.common.collect.Lists;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import org.apache.hadoop.yarn.api.ApplicationConstants;
+import org.apache.hadoop.yarn.api.records.Container;
+import org.apache.hadoop.yarn.api.records.ContainerLaunchContext;
+import org.apache.hadoop.yarn.api.records.ContainerStatus;
+import org.apache.hadoop.yarn.api.records.NodeReport;
+import org.apache.hadoop.yarn.client.api.NMClient;
+import org.apache.hadoop.yarn.client.api.async.AMRMClientAsync;
+import org.apache.hadoop.yarn.exceptions.YarnException;
+import org.apache.hadoop.yarn.util.Records;
+
+/**
+ *
+ * @author Dosky
+ */
+public class ApplicationMasterRMCallback implements AMRMClientAsync.CallbackHandler {
+
+    private Container mainContainer = null;
+    private int lauchedContainers;
+    private int allocatedContainers;
+
+    private final String hdfs_dir;
+    private final int askedContainers;
+    private final String main;
+    private final List<String> args;
+    private String taskServer;
+    private String jobManager;
+    private int numContainersToWaitFor = -1;
+
+    private NMClient nmClient;
+
+    public ApplicationMasterRMCallback(String hdfs_dir, int askedContainers, String main, List<String> args) {
+        this.hdfs_dir = hdfs_dir;
+        this.askedContainers = askedContainers;
+        this.main = main;
+        this.args = args;
+    }
+
+    void setup(NMClient nmClient) {
+        this.nmClient = nmClient;
+    }
+
+    @Override
+    public void onContainersAllocated(List<Container> containers) {
+        synchronized (this) {
+            if (numContainersToWaitFor == -1)
+                numContainersToWaitFor = 0;
+            numContainersToWaitFor += containers.size();
+        }
+        // look for last container for main
+        for (Container cont : containers) {
+            if (++allocatedContainers == askedContainers) {
+                mainContainer = cont;
+            }
+        }
+
+        for (Container container : containers) {
+
+            String mainStarter = "";
+            // master container, who will start the main
+            if (container == mainContainer) {
+                String argsString = "";
+                for (String s : args) {
+                    argsString += s + " ";
+                }
+                
+                mainStarter = " -main "
+                        + " -mainClass " + main + " " + argsString;
+            }
+
+            System.out.println("[AM] Starting client");
+            // Launch container by create ContainerLaunchContext
+            ContainerLaunchContext ctx
+                    = Records.newRecord(ContainerLaunchContext.class);
+            List script = Lists.newArrayList(
+                    "hdfs dfs -copyToLocal " + hdfs_dir + "/pop-app.jar"
+                    + ";",
+                    "hdfs dfs -copyToLocal " + hdfs_dir + "/popjava.jar"
+                    + ";",
+                    "sleep 3"
+                    + ";",
+                    "$JAVA_HOME/bin/java"
+                    + " -javaagent:popjava.jar"
+                    + " -cp popjava.jar:pop-app.jar"
+                    + " popjava.yarn.YARNContainer"
+                    + " -taskServer " + taskServer
+                    + " -jobmanager " + jobManager
+                    + " " + mainStarter
+                    + " 1>>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout"
+                    + " 2>>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr"
+                    + ";"
+            );
+            System.out.println("[AM] Executing: " + Arrays.toString(script.toArray(new String[0])));
+            ctx.setCommands(script);
+
+            System.out.println("[AM] Launching container " + container.getId());
+            try {
+                nmClient.startContainer(container, ctx);
+                lauchedContainers++;
+            } catch (YarnException | IOException ex) {
+                System.err.println("[AM] Error launching container " + container.getId() + " " + ex);
+            }
+        }
+    }
+
+    @Override
+    public void onContainersCompleted(List<ContainerStatus> statuses) {
+        for (ContainerStatus status : statuses) {
+            System.out.println("[AM] Completed container " + status.getContainerId());
+            synchronized (this) {
+                numContainersToWaitFor--;
+            }
+        }
+    }
+
+    @Override
+    public void onNodesUpdated(List<NodeReport> updated) {
+    }
+
+    @Override
+    public void onShutdownRequest() {
+    }
+
+    @Override
+    public void onError(Throwable t) {
+        t.printStackTrace();
+    }
+
+    @Override
+    public float getProgress() {
+        if(lauchedContainers == 0)
+            return 0f;
+        return askedContainers / (float) lauchedContainers;
+    }
+
+    public boolean doneWithContainers() {
+        synchronized (this) {
+            return numContainersToWaitFor == 0;
+        }
+    }
+}
