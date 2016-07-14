646a83721d8a6cf90b02a7d5b01de224b3202fba
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-14 11:26:32 -0700

    Try do simple POPJava in AM

--- popjava-yarn/src/main/java/popjava/yarn/ApplicationMasterAsync.java
+++ popjava-yarn/src/main/java/popjava/yarn/ApplicationMasterAsync.java
@@ -1,6 +1,9 @@
 package popjava.yarn;
 
+import popjava.annotation.POPAsyncConc;
 import popjava.annotation.POPClass;
+import popjava.annotation.POPObjectDescription;
+import popjava.annotation.POPSyncConc;
 
 /**
  * This class implements a simple async app master.
@@ -8,11 +11,28 @@ import popjava.annotation.POPClass;
 @POPClass(isDistributable = false)
 public class ApplicationMasterAsync {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         for(String s : args)
             System.out.println(s);
-        ApplicationMasterPOP master = new ApplicationMasterPOP(args);
-        master.setup();
-        master.runMainLoop();
+//        ApplicationMasterPOP master = new ApplicationMasterPOP(args);
+//        master.setup();
+//        master.runMainLoop();
+        A a = new A();
+        a.b();
+        
+        Thread.sleep(1000);
+    }
+    
+    @POPClass
+    public static class A {
+
+        @POPObjectDescription(url = "localhost")
+        public A() {
+        }
+        
+        @POPAsyncConc
+        public void b() {
+            System.out.println(":=|");
+        }
     }
 }
