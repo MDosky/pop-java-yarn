282c26a6f4f89ec7787b2505e4f82ab06b58f36b
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-14 10:02:42 -0700

    Manually start popjava

--- popjava-yarn/src/main/java/popjava/yarn/ApplicationMasterAsync.java
+++ popjava-yarn/src/main/java/popjava/yarn/ApplicationMasterAsync.java
@@ -2,6 +2,7 @@ package popjava.yarn;
 
 import com.beust.jcommander.JCommander;
 import popjava.annotation.POPClass;
+import popjava.system.POPSystem;
 
 /**
  * This class implements a simple async app master.
@@ -10,8 +11,10 @@ import popjava.annotation.POPClass;
 public class ApplicationMasterAsync {
 
     public static void main(String... args) throws Exception {
+        args = POPSystem.initialize(args);
         ApplicationMasterPOP master = new ApplicationMasterPOP(args);
         master.setup();
         master.runMainLoop();
+        POPSystem.end();
     }
 }
