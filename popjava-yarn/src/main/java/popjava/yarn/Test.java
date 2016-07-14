1f32f07906f48b03242b2f100b5e26b496bad2e4
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-14 11:32:11 -0700

    fix

--- popjava-yarn/src/main/java/popjava/yarn/Test.java
+++ popjava-yarn/src/main/java/popjava/yarn/Test.java
@@ -14,26 +14,29 @@ import popjava.annotation.POPObjectDescription;
  *
  * @author Dosky
  */
+@POPClass(isDistributable = false)
 public class Test {
-        public static void main(String[] args) throws InterruptedException {
-        for(String s : args)
+
+    public static void main(String[] args) throws InterruptedException {
+        for (String s : args) {
             System.out.println(s);
+        }
 //        ApplicationMasterPOP master = new ApplicationMasterPOP(args);
 //        master.setup();
 //        master.runMainLoop();
         A a = new A();
         PopJava.getThis(a).b();
-        
+
         Thread.sleep(1000);
     }
-    
+
     @POPClass
     public static class A {
 
         @POPObjectDescription(url = "localhost")
         public A() {
         }
-        
+
         @POPAsyncConc
         public void b() {
             System.out.println(":=|");
