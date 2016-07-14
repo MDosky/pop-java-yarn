933ed9400c48c68829efee331290703430f8b1eb
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-14 11:36:22 -0700

    move to other package

--- yarn-example/src/main/java/ch/heia/pop/yarn/example/Test.java
+++ yarn-example/src/main/java/ch/heia/pop/yarn/example/Test.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package popjava.yarn;
+package ch.heia.pop.yarn.example;
 
 import popjava.PopJava;
 import popjava.annotation.POPAsyncConc;
