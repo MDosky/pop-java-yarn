cbc581534e8865599f83917d2e1f0b44ef357e7d
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-14 10:49:33 -0700

    wait for link to be ready

--- popjava-yarn/src/main/java/popjava/yarn/YARNClient.java
+++ popjava-yarn/src/main/java/popjava/yarn/YARNClient.java
@@ -2,6 +2,7 @@ package popjava.yarn;
 
 import com.beust.jcommander.JCommander;
 import com.beust.jcommander.Parameter;
+import com.beust.jcommander.internal.Lists;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
@@ -74,10 +75,13 @@ public class YARNClient {
         ContainerLaunchContext amContainer
                 = Records.newRecord(ContainerLaunchContext.class);
         amContainer.setCommands(
-                Collections.singletonList(
+                Lists.newArrayList(
+                        "sleep 3"
+                        + ";",
+                        "ls -la"
+                        + ";",
                         "$JAVA_HOME/bin/java"
                         + " -javaagent:popjava.jar"
-                        + " -cp popjava.jar:pop-app.jar"
                         + " popjava.yarn.ApplicationMasterAsync"
                         + " --dir " + hdfs_dir
                         + " --vcores " + vcores
@@ -87,6 +91,7 @@ public class YARNClient {
                         + " " + argsString
                         + " 1>>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout"
                         + " 2>>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr"
+                        + ";"
                 )
         );
 
