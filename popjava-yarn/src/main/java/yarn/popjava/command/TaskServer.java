5e3398031df51c7b528e0caa3bc148dd3ea04e76
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-07-18 10:27:06 -0700

    Back to simple class

--- popjava-yarn/src/main/java/yarn/popjava/command/TaskServer.java
+++ popjava-yarn/src/main/java/yarn/popjava/command/TaskServer.java
@@ -4,10 +4,12 @@ import popjava.PopJava;
 import popjava.annotation.POPClass;
 import popjava.annotation.POPObjectDescription;
 import popjava.annotation.POPParameter;
+import popjava.annotation.POPSyncMutex;
 import popjava.annotation.POPSyncSeq;
 import popjava.base.POPObject;
 import popjava.jobmanager.POPJavaJobManager;
 import popjava.jobmanager.ServiceConnector;
+import popjava.service.DaemonInfo;
 
 /**
  *
@@ -28,8 +30,8 @@ public class TaskServer extends POPObject {
         this.jm = jm;
     }
     
-    @POPSyncSeq
-    public void registerDaemon(@POPParameter(POPParameter.Direction.INOUT) ServiceConnector di) {
+    @POPSyncMutex
+    public void registerDaemon(@POPParameter(POPParameter.Direction.INOUT) DaemonInfo di) {
         jm.registerService(di);
         System.out.println("[TS] Registering service " + di);
     }
