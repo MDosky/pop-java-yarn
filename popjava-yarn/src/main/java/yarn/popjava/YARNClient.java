c3267b45880154e48cbf4eb68e9e826795ae8ea8
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-08-02 10:35:47 -0700

    back

--- popjava-yarn/src/main/java/yarn/popjava/YARNClient.java
+++ popjava-yarn/src/main/java/yarn/popjava/YARNClient.java
@@ -84,8 +84,6 @@ public class YARNClient {
         ContainerLaunchContext amContainer
                 = Records.newRecord(ContainerLaunchContext.class);
         amContainer.setCommands(Lists.newArrayList(
-                        "if [ -e /tmp/AMdump ]; then hdfs dfs -copyFromLocal /tmp/AMdump /tmp/AMdump; rm /tmp/AMdump; fi;",
-                
                           "$JAVA_HOME/bin/java"
                         + " -Xmx" + (memory / 2) + "M"
                         + " -XX:+HeapDumpOnOutOfMemoryError"
@@ -100,7 +98,8 @@ public class YARNClient {
                         + " " + argsString
                         + " 1>>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stdout"
                         + " 2>>" + ApplicationConstants.LOG_DIR_EXPANSION_VAR + "/stderr"
-                        + ";"
+                        + ";",
+                        "if [ -e /tmp/AMdump ]; then hdfs dfs -copyFromLocal /tmp/AMdump /tmp/AMdump; rm /tmp/AMdump; fi;"
                 )
         );
 
