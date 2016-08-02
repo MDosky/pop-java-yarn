0b46bd74b1a0ea0c8f22f94f8a23708ae8f0b0f2
Davide Mazzoleni <davide.mazzoleni@edu.hefr.ch>
2016-08-02 09:54:17 -0700

    More objects and container out

--- yarn-example/src/main/java/ch/heia/pop/yarn/example/JMAllocation.java
+++ yarn-example/src/main/java/ch/heia/pop/yarn/example/JMAllocation.java
@@ -13,35 +13,29 @@ public class JMAllocation {
 
     public static void main(String[] args) {
         System.out.println("Starting pop java app");
-        
+
         MyAllocator instance;
 
-        System.out.println("First");
-        instance = new MyAllocator();
-        report(instance.getMillis());
+        int n;
+        try {
+            n = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            n = 20;
+        }
 
-        System.out.println("Second");
-        instance = new MyAllocator();
-        report(instance.getMillis());
-
-        System.out.println("Third");
-        instance = new MyAllocator();
-        report(instance.getMillis());
-
-        System.out.println("Forth");
-        instance = new MyAllocator();
-        report(instance.getMillis());
-
-        System.out.println("Fifth");
-        instance = new MyAllocator();
-        report(instance.getMillis());
+        for (int i = 0; i < n; i++) {
+            System.out.println("Run " + i);
+            instance = new MyAllocator();
+            report(instance.getMillis(), instance.getContainer());
+        }
 
         System.out.println("End app");
     }
-    
-    private static void report(long millis) {
+
+    private static void report(long millis, String container) {
         System.out.println("Remote Obj reported: " + new Date(millis));
         System.out.println("Server time is     : " + new Date());
+        System.out.println("Container is       : " + container);
         System.out.println();
     }
 
@@ -54,6 +48,11 @@ public class JMAllocation {
         @POPSyncSeq
         public long getMillis() {
             return System.currentTimeMillis();
+        }
+
+        @POPSyncSeq
+        public String getContainer() {
+            return System.getenv("CONTAINER_ID");
         }
     }
 
